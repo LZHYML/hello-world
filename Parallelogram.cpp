7f87fa89586defeaf4d16b6c8ad8054bee53fc3d
LZHYML <37575232+LZHYML@users.noreply.github.com>
2018-03-30 10:44:02 +0800

    Update Parallelogram.cpp

--- Parallelogram.cpp
+++ Parallelogram.cpp
@@ -1,5 +1,19 @@
+#include <iostream>
+#include <math.h>
+#define PI 3.1415926
+using namespace std;
+
 class Parallelogram {
+  public:
+  Parallelogram();
+  void show();
+  void area();
+  void height();
+  void length();
+  private:
+  double side1, side2, radian;
 };
+
 int main() {
   Parallelogram pg;
   pg.show();
@@ -7,4 +21,37 @@ int main() {
   pg.height();
   pg.length();
   return 0;
+}
+
+Parallelogram::Parallelogram()
+{
+  side1 = 8;
+  side2 = 6*sqrt(2);
+  radian = PI/4;
+}
+
+void Parallelogram::show()
+{
+  cout<<"********"<<endl;
+  cout<<"  ********"<<endl;
+  cout<<"    ********"<<endl;
+  cout<<"      ********"<<endl;
+}
+
+void Parallelogram::area()
+{
+  double area = side1 * side2 * sin(radian);
+  cout<<"平行四边形面积是"<<area<<endl;
+}
+
+void Parallelogram::height()
+{
+  double height = side2 * sin(radian);
+  cout<<"平行四边形的高为"<<height<<endl;
+}
+
+void Parallelogram::length()
+{
+  double length = (side1 + side2) * 2;
+  cout<<"平行四边形的边长为"<<length<<endl;
 }
